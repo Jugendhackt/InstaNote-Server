aeff33bc62800fcccd7fad7de5ed4b4b039b057a
xCubeArrow <martinharald.goetze@gmail.com>
2018-10-20 11:12:56 +0200

    Simple HttpServer without any Contexts

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,36 @@
+import com.sun.net.httpserver.Headers;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Hello World");
+	private Main() {
+		try {
+			HttpServer httpServer = HttpServer.create(new InetSocketAddress(2000), 0);
+
+
+			httpServer.start();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
+
+	public static void main(String[] args) {
+		new Main();
+	}
+
+
+	void write(String text, int rCode, HttpExchange exchange) throws IOException {
+		Headers responseHeaders = exchange.getResponseHeaders();
+		responseHeaders.add("Content-Type", "application/json");
+
+		exchange.sendResponseHeaders(rCode, text.length());
+		exchange.getResponseBody().write(text.getBytes());
+		exchange.getResponseBody().close();
+	}
+
+
 }
+
