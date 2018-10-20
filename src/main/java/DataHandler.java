028d40130fb4104e668c126a86c7469e480c1e35
juweske <julian.weske@gmx.de>
2018-10-20 18:18:28 +0200

    Added Description to query

--- src/main/java/DataHandler.java
+++ src/main/java/DataHandler.java
@@ -39,15 +39,15 @@ class DataHandler {
         //Get the entities id based on a query
         String entityId = queryToEntity(query, lang);
         String queryAufruf = "SELECT ?Name ?EinwohnerZahl ?Landeswappen ?Karte ?Bild ?Koordinaten ?Flagge ?description WHERE {\n" +
-                "  wd:"+entityId+" wdt:P1448 ?Name.\n" +
-                "  OPTIONAL { wd:"+entityId+" wdt:P1082 ?EinwohnerZahl. }\n" +
-                "  OPTIONAL { wd:"+entityId+" wdt:P94 ?Landeswappen. }\n" +
-                "  OPTIONAL { wd:"+entityId+" wdt:P242 ?Karte. }\n" +
-                "  OPTIONAL { wd:"+entityId+" wdt:P18 ?Bild. }\n" +
-                "  OPTIONAL { wd:"+entityId+" wdt:P625 ?Koordinaten. }\n" +
-                "  OPTIONAL { wd:"+entityId+" wdt:P41 ?Flagge. }\n" +
-                "  OPTIONAL { wd:"+entityId+" schema:description ?description.\n" +
-                "           FILTER(LANG(?description)=\"de\")}\n" +
+                "  wd:"+entityId+" wdt:P1448 ?Name." +
+                "  OPTIONAL { wd:"+entityId+" wdt:P1082 ?EinwohnerZahl. }" +
+                "  OPTIONAL { wd:"+entityId+" wdt:P94 ?Landeswappen. }" +
+                "  OPTIONAL { wd:"+entityId+" wdt:P242 ?Karte. }" +
+                "  OPTIONAL { wd:"+entityId+" wdt:P18 ?Bild. }" +
+                "  OPTIONAL { wd:"+entityId+" wdt:P625 ?Koordinaten. }" +
+                "  OPTIONAL { wd:"+entityId+" wdt:P41 ?Flagge. }" +
+                "  OPTIONAL { wd:"+entityId+" schema:description ?description." +
+                "           FILTER(LANG(?description)=\"de\")}" +
                 "  SERVICE wikibase:label { bd:serviceParam wikibase:language \"[AUTO_LANGUAGE],en\". }\n" +
                 "}";
         try {
